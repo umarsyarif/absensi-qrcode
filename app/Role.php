824f5dbd16d3d67f691d9aca6be296bd43e4078c
umarsyarif <11750115076@students.uin-suska.ac.id>
2020-05-06 01:01:11 +0700

    relation model to role

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->hasOne('App\Role');
+    }
 }
