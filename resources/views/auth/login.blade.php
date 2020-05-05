c9ce3517205ecd0b9a359a7620f06ff33dff3df4
umarsyarif <11750115076@students.uin-suska.ac.id>
2020-05-06 00:50:07 +0700

    alert id/pass salah

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,10 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        @if (session('error'))
+                        <div class="alert alert-danger">{{ session('error') }}</div>
+                        @endif
+
                         <div class="form-group row">
                             <label for="identity" class="col-md-4 col-form-label text-md-right">{{ __('ID') }}</label>
 
