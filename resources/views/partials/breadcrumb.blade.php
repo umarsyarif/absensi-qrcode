566d0f9bb5346655ec421710c5ecf1157c87b2a2
umarsyarif <11750115076@students.uin-suska.ac.id>
2020-06-02 01:32:32 +0700

    breadcrumb

--- resources/views/partials/breadcrumb.blade.php
+++ resources/views/partials/breadcrumb.blade.php
@@ -3,7 +3,7 @@ $data = $breadcrumbs ?? [];
 ?>
 <div class="col-sm-6">
     <ol class="breadcrumb float-sm-right">
-        <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
         @foreach ($data as $key => $breadcrumb)
             @if(\Illuminate\Support\Facades\Route::has($key))
                 <li class="breadcrumb-item"><a href="{{route($key)}}"><?=$breadcrumb?></a></li>
